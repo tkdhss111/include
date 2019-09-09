a4f57ce3368219d2e891f4e5e83fc457edf16de0
tkdhss111 <tkdhss111@gmail.com>
2019-09-09 09:55:41 +0900

    mkl

--- mkl.mk
+++ mkl.mk
@@ -1,5 +1,8 @@
-DIR_MKL      := /opt/intel/mkl/
-DIRS_INC_RLS += $(addprefix $(DIR_MKL), include/intel64/lp64 include)
-LIBS_RLS     += $(addprefix ${DIR_MKL}/lib/intel64/, libmkl_blas95_lp64.a libmkl_lapack95_lp64.a libmkl_gf_lp64.a libmkl_gnu_thread.a libmkl_core.a)
-LIBS_RLS     += -Wl,--start-group -Wl,--end-group -lgomp -lpthread -lm -ldl 
-#CFLAGS        += -fexternal-blas
+MKLROOT := /opt/intel/mkl
+F95ROOT := /home/jma/2_Tools/mkl
+
+LIBS_RLS +=$(F95ROOT)/lib/intel64/libmkl_blas95_lp64.a $(F95ROOT)/lib/intel64/libmkl_lapack95_lp64.a -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_gf_lp64.a $(MKLROOT)/lib/intel64/libmkl_intel_thread.a $(MKLROOT)/lib/intel64/libmkl_core.a -Wl,--end-group -lgomp5 -lpthread -lm -ldl
+
+DIRS_INC_RLS +=$(F95ROOT)/include/intel64/lp64 $(MKLROOT)/include
+
+#CFLAGS += -fexternal-blas
