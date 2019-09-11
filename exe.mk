be0cecede7e0596379c8b91e3eebb83963599651
tkdhss111 <tkdhss111@gmail.com>
2019-09-11 16:11:59 +0900

    a little enhancement

--- exe.mk
+++ exe.mk
@@ -8,8 +8,6 @@
 #=============================================================
 
 include ~/1_Projects/include/base.mk
-include ~/1_Projects/include/caf.mk
 include ~/1_Projects/include/utils.mk
 include ~/1_Projects/include/mkl.mk
-include ~/1_Projects/include/dislin.mk
 include ~/1_Projects/include/main.mk
