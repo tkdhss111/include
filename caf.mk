da1f9be82c2873f5863faf032c54b227489529e1
tkdhss111 <tkdhss111@gmail.com>
2020-01-17 22:50:36 +0900

    del caf

--- caf.mk
+++
@@ -1,8 +0,0 @@
-FC := caf
-CFLAGS += -Dcaf
-
-# Overwrite
-DIRS_INC_RLS = $(DIR_PROJS)utils_caf/lib/Release/
-DIRS_INC_DBG = $(DIR_PROJS)utils_caf/lib/Debug/
-LIBS_RLS = $(DIR_PROJS)utils_caf/lib/Release/libutils_caf.a
-LIBS_DBG = $(DIR_PROJS)utils_caf/lib/Debug/libutils_caf.a
