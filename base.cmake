c58832fef9ec560ee7d1b1a216929d6e2dfb1d5e
tkdhss111 <tkdhss111@gmail.com>
2020-07-26 06:37:57 +0900

    cmake caf

---
+++ base.cmake
@@ -0,0 +1,23 @@
+#cmake_minimum_required ( VERSION 3.0 )
+#project ( hello LANGUAGES Fortran VERSION 1.0.0 )
+
+enable_language ( Fortran )
+enable_testing ()
+
+if ( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
+  set ( dialect "-cpp -ffree-line-length-none -Dlinux" )
+  set ( bounds "-fbounds-check -Ddebug")
+endif ()
+
+if ( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
+  set ( dialect "-cpp -free -Dlinux -Dintel" )
+  set ( bounds "-check bounds -Ddebug" )
+endif ()
+
+if ( CMAKE_Fortran_COMPILER_ID MATCHES "PGI" )
+  set ( dialect "-Mfreeform -Mdclchk -Mstandard -Mallocatable=03" )
+  set ( bounds "-C" )
+endif ()
+
+set ( CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} ${bounds}" )
+set ( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${dialect}" )
