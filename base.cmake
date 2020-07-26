2f0b84744f8f6ed381d9125048a701668bce86ef
tkdhss111 <tkdhss111@gmail.com>
2020-07-26 19:47:16 +0900

    cmake

--- base.cmake
+++ base.cmake
@@ -5,13 +5,13 @@ enable_language ( Fortran )
 enable_testing ()
 
 if ( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
-  set ( dialect "-cpp -ffree-line-length-none -Dlinux" )
-  set ( bounds "-fbounds-check -Ddebug")
+  set ( dialect "-cpp -ffree-line-length-none -Dlinux -fcoarray=lib" )
+  set ( bounds "-fbounds-check -Ddebug -fcoarray=lib")
 endif ()
 
 if ( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
-  set ( dialect "-cpp -free -Dlinux -Dintel" )
-  set ( bounds "-check bounds -Ddebug" )
+  set ( dialect "-cpp -free -Dlinux -Dintel -coarray=shared" )
+  set ( bounds "-check bounds -Ddebug -coarray=shared" )
 endif ()
 
 if ( CMAKE_Fortran_COMPILER_ID MATCHES "PGI" )
--- cmake.mk
+++ cmake.mk
@@ -2,7 +2,7 @@ DIR_INS=${HOME}/local/
 DIR_BIN=${DIR_INS}bin/
 DIR_LIB=${DIR_INS}lib/
 
-CMAKE=FC=caf cmake .. -GNinja -DCMAKE_INSTALL_PREFIX=${DIR_INS}
+CMAKE=cmake .. -GNinja -DCMAKE_INSTALL_PREFIX=${DIR_INS}
 CMAKE_RLS=$(CMAKE) -DCMAKE_BUILD_TYPE=Release
 CMAKE_DBG=$(CMAKE) -DCMAKE_BUILD_TYPE=Debug
 
@@ -19,16 +19,34 @@ debugrun: debug
 run: release
 	${DIR_BIN}$(BIN)
 
-.PHONY: tests
-tests:
-	$(MKDIR_BUILD) && $(CMAKE_DBG) && ninja && ctest -VV
+run_intel: release_intel
+	${DIR_BIN}$(BIN)
+
+.PHONY: test
+test:
+	export FC=caf && \
+	$(MKDIR_BUILD) && $(CMAKE_DBG) && $(NINJA) && ctest -VV
+
+.PHONY: test_intel
+test_intel:
+	source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh -arch intel64 -platform linux && \
+	export FC=ifort CC=icc CXX=icpc && \
+	$(MKDIR_BUILD) && $(CMAKE_DBG) && $(NINJA) && ctest -VV
 
 .PHONY: debug
 debug:
-	$(MKDIR_BUILD) && $(CMAKE_DBG) && $(NINJA)
+	export FC=caf && \
+	$(MKDIR_BUILD) && $(CMAKE_DBG) && $(NINJA) && $(NINJA) install
+
+.PHONY: debug_intel
+debug_intel:
+	source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh -arch intel64 -platform linux && \
+	export FC=ifort CC=icc CXX=icpc && \
+	$(MKDIR_BUILD) && $(CMAKE_DBG) && $(NINJA) && $(NINJA) install
 
 .PHONY: release
 release:
+	export FC=caf && \
 	$(MKDIR_BUILD) && $(CMAKE_RLS) && $(NINJA) && $(NINJA) install
 
 .PHONY: release_intel
@@ -38,7 +56,7 @@ release_intel:
 	$(MKDIR_BUILD) && $(CMAKE_RLS) && $(NINJA) && $(NINJA) install
 
 clean:
-	rm -r build ${HOME}/local/bin/${BIN}
+	rm -r build ${HOME}/local/bin/${BIN} ${HOME}/local/debug/${BIN}
 
 clean_lib:
 	rm -r build ${HOME}/local/lib/${LIB}
